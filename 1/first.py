e8ed3f622696a86bcea97ebdd37fae1b8f4ba3ba
Gaupemor <selmafs@student.matnat.uio.no>
2019-12-01 15:55:22 +0100

    1: Reformatting

--- 1/first.py
+++ 1/first.py
@@ -4,12 +4,17 @@ import numpy as np
 Day 1 - Puzzle 1 & 2
 https://adventofcode.com/2019/day/1
 """
-module_masses = np.loadtxt(fname = "input.txt")
 
+module_masses = np.loadtxt(fname = "input.txt")
 module_total_fuel_req = complete_total_fuel_req = 0
 
+
+def calc_fuel_req(mass):
+    return int(mass / 3) - 2
+
+
 for module_mass in module_masses:
-    module_fuel_req = int(module_mass / 3) - 2
+    module_fuel_req = calc_fuel_req(module_mass)
 
     # Part 1
     module_total_fuel_req += module_fuel_req
@@ -17,10 +22,8 @@ for module_mass in module_masses:
     # Part 2
     while module_fuel_req > 0:
         complete_total_fuel_req += module_fuel_req
-        module_fuel_req = int(module_fuel_req / 3) - 2
+        module_fuel_req = calc_fuel_req(module_fuel_req)
 
 
-print(f"1: Module total fuel requirements: {module_total_fuel_req}")
-# 3254441
-print(f"2: Complete total fuel requirements: {complete_total_fuel_req}")
-# 4878818
+print(f"1: Module total fuel requirements: {module_total_fuel_req}") # 3254441
+print(f"2: Complete total fuel requirements: {complete_total_fuel_req}") # 4878818
